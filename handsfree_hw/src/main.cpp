dee783c16f38c0d9db300a3b8a31121dad255ce4
mawenke <315261982@qq.com>
2016-05-04 21:26:07 +0800

    change x

--- handsfree_hw/src/main.cpp
+++ handsfree_hw/src/main.cpp
@@ -2,9 +2,9 @@
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "robothw");
-	ros::NodeHandle nh("handsfree");
-  handsfree_hw::HF_HW_ros hf(nh, "serial:///dev/ttyUSB0", "/home/kedou/ros_workspace/HANDS_FREE_WS/src/handsfree_hw/config.txt");
-	hf.mainloop();
-	return 0;
+    ros::init(argc, argv, "robothw");
+    ros::NodeHandle nh("handsfree");
+    handsfree_hw::HF_HW_ros hf(nh, "serial:///dev/ttyUSB0", "/home/kedou/ros_workspace/HANDS_FREE_WS/src/handsfree_hw/config.txt");
+    hf.mainloop();
+    return 0;
 }

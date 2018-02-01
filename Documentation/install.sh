9e8133920d4f5b23fa830929b12d0165a4cc66c3
chujitaozi <1484548330@qq.com>
2018-02-01 15:53:38 +0800

    fix bug

--- Documentation/install.sh
+++ Documentation/install.sh
@@ -3,17 +3,19 @@
 #安装HandsFree ROS包
 echo 请确保已经成功安装indigo
 sleep 1
-mkdir -p /opt/ros_handsfree/src
-git clone https://github.com/HANDS-FREE/handsfree.git /opt/ros_handsfree/src
-cd /opt/ros_handsfree/src/Documentation
+mkdir -p ~/ros_handsfree/src
+git clone https://github.com/HANDS-FREE/handsfree.git ~/ros_handsfree/src
+cd ~/ros_handsfree/src/Documentation
 sh environment_config.sh -y -y
 sh rbx1-prereq.sh
 sh rbx2-prereq.sh
-cd ../handsfree_hw/src/
-find . -name 'main.cpp' | xargs perl -pi -e 's|/home/kedou/ros_workspace/mobile_robot_ws/src/handsfree/handsfree_hw|/opt/ros_handsfree/src/handsfree_hw|g'
-cd /opt/ros_handsfree/
+cd ../handsfree_hw
+pathofhf=`pwd`
+cd src/
+sed -i "s|/home/kedou/ros_workspace/mobile_robot_ws/src/handsfree/handsfree_hw|$pathofhf|" main.cpp
+cd ~/ros_handsfree/
 catkin_make
-echo "source /opt/ros_handsfree/devel/setup.sh" >> ~/.bashrc
+echo "source ~/ros_handsfree/devel/setup.sh" >> ~/.bashrc
 echo HandsFree 相关程序已经安装完毕
-echo 安装路径为：/opt/ros_handsfree/src
+echo 安装路径为：~/ros_handsfree/src
 

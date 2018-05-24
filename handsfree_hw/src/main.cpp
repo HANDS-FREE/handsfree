1b89fe89228be3f10a051d02947549b41af2a9ec
mawenke <315261982@qq.com>
2018-05-24 17:48:47 +0800

    update rplidar_ros

--- handsfree_hw/src/main.cpp
+++ handsfree_hw/src/main.cpp
@@ -4,17 +4,10 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "robothw");
     ros::NodeHandle nh("handsfree");
-
-    FILE *pf = popen("rospack find handsfree_hw", "r");
-    char res[1024];
-    fread(res,1024, 1, pf);
-    pclose(pf);
-    std::string configfile=res;
-    configfile.erase(configfile.end() - 1);
-    configfile=configfile+"/config.txt";
-    std::cerr << "config path:" << configfile <<std::endl;
-
-    handsfree_hw::HF_HW_ros hf(nh, "serial:///dev/ttyUSB0", configfile);
+    std::string config_filename = "/config.txt" ;
+    std::string config_filepath = config_path+config_filename ; 
+    std::cerr<<"the configure file path is: "<<config_filepath<<std::endl ; 
+    handsfree_hw::HF_HW_ros hf(nh, "serial:///dev/ttyUSB0", config_filepath);
     hf.mainloop();
     return 0;
 }

9e0d571167e4462749dd81cd6fa71cea091a4b86
mawenke <315261982@qq.com>
2018-05-26 15:09:09 +0800

    fix tf bug and add env.sh

---
+++ handsfree_hw/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if grep -q "HANDSFREE_ROBOT_MODEL"  ~/.bashrc 
+then   
+echo -e "\n" 
+else  
+echo "### MODEL type [mini, stone_v2, stone_v3, giraffe]" >> ~/.bashrc
+echo "export HANDSFREE_ROBOT_MODEL=stone_v2" >> ~/.bashrc
+echo -e "\n" 
+fi  
+

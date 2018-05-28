8611540beb852cee956295bb921d408db06c6b8e
mawenke <315261982@qq.com>
2018-05-28 15:28:29 +0800

    add auto_install_all.sh

---
+++ Documentation/auto_install_all_ubuntu16.04.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git
+mkdir -p ~/handsfree/handsfree_ros_ws/src/
+cd ~/handsfree/handsfree_ros_ws/src/
+echo 设置 HandsFree 安装路径为： ~/handsfree
+git clone https://github.com/HANDS-FREE/handsfree.git
+cd ~/handsfree/handsfree_ros_ws/src/handsfree/Documentation
+sudo cp ./script/sources_ubuntu_16_04.list /etc/apt/sources.list
+sudo apt-get update
+echo 设置 HandsFree 开发环境
+sh ./script/auto_set_ubuntu16.04.sh
+echo 安装 ROS 基本环境
+sh ./script/ros_kinetic_base.sh
+echo 安装 ROS 扩展环境
+sh ./script/ros_kinetic_ext.sh
+
+echo 嵌入式开发库 OpenRE 
+cd ~/handsfree
+git clone https://github.com/HANDS-FREE/OpenRE.git
+
+echo 安装 HandsFree ROS 
+cd ~/handsfree/handsfree_ros_ws/src/
+catkin_init_workspace
+cd ./handsfree/handsfree_ros_ws
+catkin_make
+echo "source ~/handsfree/handsfree_ros_ws/devel/setup.sh" >> ~/.bashrc
+source ~/.bashrc
+echo HandsFree 相关程序已经安装完毕
+echo 安装路径为：~/handsfree
--- Documentation/script/ros_indigo_base.sh
+++ Documentation/script/ros_indigo_base.sh
@@ -2,7 +2,7 @@
 
 install_command="sudo apt-get install -y"
 
-sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c '. /etc/lsb-release && echo "deb http://ros.exbot.net/rospackage/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 $install_command ros-indigo-desktop-full

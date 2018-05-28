0853116aced77a99d19bfbe2e926206e33b22059
mawenke <315261982@qq.com>
2018-05-28 16:02:59 +0800

    fix auto_install_all.sh

--- Documentation/auto_install_all_ubuntu14.04.sh
+++ Documentation/auto_install_all_ubuntu14.04.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get update
 sudo apt-get install -y git
-mkdir -p ~/handsfree/handsfree_ros_ws/src/
-cd ~/handsfree/handsfree_ros_ws/src/
+mkdir -p ~/handsfree/handsfree_ros_ws/
+cd ~/handsfree/handsfree_ros_ws/
 echo 设置 HandsFree 安装路径为： ~/handsfree
-git clone https://github.com/HANDS-FREE/handsfree.git
-cd ~/handsfree/handsfree_ros_ws/src/handsfree/Documentation
+git clone https://github.com/HANDS-FREE/handsfree.git src
+cd ~/handsfree/handsfree_ros_ws/src/Documentation
 sudo cp ./script/sources_ubuntu_14_04.list /etc/apt/sources.list
 sudo apt-get update
 echo 设置 HandsFree 开发环境
@@ -16,14 +16,10 @@ sh ./script/ros_indigo_base.sh
 echo 安装 ROS 扩展环境
 sh ./script/ros_indigo_ext.sh
 
-echo 嵌入式开发库 OpenRE 
-cd ~/handsfree
-git clone https://github.com/HANDS-FREE/OpenRE.git
-
 echo 安装 HandsFree ROS 
 cd ~/handsfree/handsfree_ros_ws/src/
 catkin_init_workspace
-cd ./handsfree/handsfree_ros_ws
+cd ~/handsfree/handsfree_ros_ws
 catkin_make
 echo "source ~/handsfree/handsfree_ros_ws/devel/setup.sh" >> ~/.bashrc
 source ~/.bashrc
--- Documentation/auto_install_all_ubuntu16.04.sh
+++ Documentation/auto_install_all_ubuntu16.04.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get update
 sudo apt-get install -y git
-mkdir -p ~/handsfree/handsfree_ros_ws/src/
-cd ~/handsfree/handsfree_ros_ws/src/
+mkdir -p ~/handsfree/handsfree_ros_ws/
+cd ~/handsfree/handsfree_ros_ws/
 echo 设置 HandsFree 安装路径为： ~/handsfree
-git clone https://github.com/HANDS-FREE/handsfree.git
-cd ~/handsfree/handsfree_ros_ws/src/handsfree/Documentation
-sudo cp ./script/sources_ubuntu_16_04.list /etc/apt/sources.list
+git clone https://github.com/HANDS-FREE/handsfree.git src
+cd ~/handsfree/handsfree_ros_ws/src/Documentation
+sudo cp ./script/sources_ubuntu_14_04.list /etc/apt/sources.list
 sudo apt-get update
 echo 设置 HandsFree 开发环境
 sh ./script/auto_set_ubuntu16.04.sh
@@ -16,14 +16,10 @@ sh ./script/ros_kinetic_base.sh
 echo 安装 ROS 扩展环境
 sh ./script/ros_kinetic_ext.sh
 
-echo 嵌入式开发库 OpenRE 
-cd ~/handsfree
-git clone https://github.com/HANDS-FREE/OpenRE.git
-
 echo 安装 HandsFree ROS 
 cd ~/handsfree/handsfree_ros_ws/src/
 catkin_init_workspace
-cd ./handsfree/handsfree_ros_ws
+cd ~/handsfree/handsfree_ros_ws
 catkin_make
 echo "source ~/handsfree/handsfree_ros_ws/devel/setup.sh" >> ~/.bashrc
 source ~/.bashrc

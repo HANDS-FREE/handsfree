32aec351aadac417081dc3ed0e99dd7434f8c64b
mawenke <315261982@qq.com>
2018-08-25 17:05:53 +0800

    fix auto_install shell

--- Documentation/auto_install_all_ubuntu14.04.sh
+++ Documentation/auto_install_all_ubuntu14.04.sh
@@ -22,23 +22,24 @@ echo 代码下载完毕...........
 echo 设置 HandsFree 开发环境
 sleep 3
 cd ~/handsfree/handsfree_ros_ws/src/handsfree/Documentation/script/
-#sh ./ubuntu_14.04_base.sh
+#bash ./ubuntu_14.04_base.sh
 echo 安装 ROS 基本环境
 sleep 3
-sh ./ros_indigo_base.sh
+bash ./ros_indigo_base.sh
 echo 安装 ROS 扩展环境
 sleep 3
-sh ./ros_indigo_ext.sh
+bash ./ros_indigo_ext.sh
 
 echo 安装 HandsFree ROS 
 sleep 3
+source ~/.bashrc
 cd ~/handsfree/handsfree_ros_ws/src/
 catkin_init_workspace
 sleep 3
 cd ~/handsfree/handsfree_ros_ws
 catkin_make
 echo "source ~/handsfree/handsfree_ros_ws/devel/setup.sh" >> ~/.bashrc
-bash ~/handsfree/handsfree_ros_ws/devel/setup.sh
+source ~/.bashrc
 echo HandsFree 相关程序已经安装完毕
 echo 安装路径为：~/handsfree
 

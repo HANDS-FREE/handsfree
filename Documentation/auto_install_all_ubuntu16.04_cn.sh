a7473e33a772be8d52af26be51fb114d868033ee
mawenke <31526192@qq.com>
2020-09-11 14:55:29 +0800

    update Documentation auto_install script

--- Documentation/auto_install_all_ubuntu16.04_cn.sh
+++ Documentation/auto_install_all_ubuntu16.04_cn.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cd ~
-wget https://raw.githubusercontent.com/HANDS-FREE/handsfree/master/Documentation/script/sources_ubuntu_14_04.list
+wget https://gitee.com/HANDS-FREE/handsfree/raw/master/Documentation/script/sources_ubuntu_16_04.list
 echo 更新系统源列表
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
-sudo cp ./sources_ubuntu_14_04.list /etc/apt/sources.list
+sudo cp ./sources_ubuntu_16_04.list /etc/apt/sources.list
+sudo apt-get update
+sleep 3
+sudo apt-get purge libappstream3
+sleep 3
 sudo apt-get update
 sleep 3
 
@@ -16,23 +20,23 @@ mkdir -p ~/handsfree/handsfree_ros_ws/src
 cd ~/handsfree/handsfree_ros_ws/src
 echo 设置 HandsFree ROS安装路径为：~/handsfree/handsfree_ros_ws/src
 echo 下载Github上HandsFree的代码............
-git clone https://github.com/HANDS-FREE/handsfree.git 
+git clone https://gitee.com/HANDS-FREE/handsfree.git
 echo 代码下载完毕...........
 
 echo 设置 HandsFree 开发环境
 sleep 3
 cd ~/handsfree/handsfree_ros_ws/src/handsfree/Documentation/script/
-#bash ./ubuntu_14.04_base.sh
+#bash ./ubuntu_16.04_base.sh
 echo 安装 ROS 基本环境
 sleep 3
-bash ./ros_indigo_base.sh
+bash ./ros_kinetic_base.sh
 echo 安装 ROS 扩展环境
 sleep 3
-bash ./ros_indigo_ext.sh
+bash ./ros_kinetic_ext.sh
 
 echo 安装 HandsFree ROS 
 sleep 3
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 cd ~/handsfree/handsfree_ros_ws/src/
 catkin_init_workspace
 sleep 3
@@ -46,5 +50,3 @@ echo 安装路径为：~/handsfree
 echo 设置usb规则
 cd ~/handsfree/handsfree_ros_ws/src/handsfree/Documentation/
 bash set_usb_env.sh
-
-sudo cp /opt/ros/indigo/share/laser_filters/laser_filters_plugins.xml /opt/ros/indigo/share/laser_filters_jsk_patch/
--- handsfree_hw/env.sh
+++ handsfree_hw/env.sh
@@ -3,8 +3,12 @@
 if grep -q "HANDSFREE_ROBOT_MODEL"  ~/.bashrc 
 then   
 echo -e "\n" 
-else  
-echo "### MODEL type [mini, stone_v2, stone_v3, giraffe]" >> ~/.bashrc
+else 
+echo "#HANDSFREE_ROBOT_MODEL: support all HandsFree open source robots" >> ~/.bashrc
+echo "#####Mini Series: mini mini_omni3 mini_mecanum4 mini_carlike" >> ~/.bashrc
+echo "#####Stone Series: stone stone_v2 stone_v3 stone_v3_omni3" >> ~/.bashrc
+echo "#####Giraffe Series: giraffe giraffe_v2" >> ~/.bashrc
+echo "###请修改HANDSFREE_ROBOT_MODEL是你正在使用的机器人型号" >> ~/.bashrc
 echo "export HANDSFREE_ROBOT_MODEL=stone_v3" >> ~/.bashrc
 echo -e "\n" 
 fi  

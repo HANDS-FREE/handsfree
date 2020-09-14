c69f3c26beed5389c7d59ed81b06ede5fcde1ba4
mawenke <31526192@qq.com>
2020-09-14 20:58:53 +0800

    add handsfree_2dslam speed_up para

---
+++ handsfree_2dslam/install_slam.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install ros-kinetic-cartographer ros-kinetic-cartographer-ros ros-kinetic-cartographer-ros-msgs ros-kinetic-cartographer-rviz
+
+sudo apt-get install ros-kinetic-hector-mapping
+
+sudo apt-get install ros-kinetic-slam-karto

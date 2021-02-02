5c422ecdc3b981f6be8c532a6ec401dce784dfc6
mawenke <31526192@qq.com>
2021-02-02 15:21:42 +0800

    fix auto_install_handsfree.sh

--- auto_install_handsfree.sh
+++ auto_install_handsfree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## auto install ros and handsfree , supports ros kinetic(ubuntu16.04), melodic(ubuntu18.04), noetic(ubuntu18.04)
+## auto install ros and handsfree , supports ros kinetic(ubuntu16.04), melodic(ubuntu18.04), noetic(ubuntu20.04)
 cd
 wget https://gitee.com/HANDS-FREE/handsfree/raw/master/Documentation/auto_install_all_ubuntu_cn.sh && bash ./auto_install_all_ubuntu_cn.sh kinetic
 

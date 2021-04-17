788c367cdc5b5feafa92ffc19cbeafc4d213426e
mawenke <31526192@qq.com>
2021-04-17 13:07:26 +0800

    add handsfree_teleop/scripts/handsfree_teleop_key.py

--- Documentation/set_usb_env.sh
+++ Documentation/set_usb_env.sh
@@ -9,3 +9,14 @@ sudo service udev restart
 
 sudo usermod -a -G dialout $USER
 
+sudo chmod 777 ../handsfree_ar_tags/nodes/*.py
+sudo chmod 777 ../handsfree_smach/script/*.py
+sudo chmod 777 ../handsfree_speech/nodes/*.py
+sudo chmod 777 ../handsfree_teleop/scripts/*.py
+sudo chmod 777 ../handsfree_tutorials/script/1_get_sensors/*.py
+sudo chmod 777 ../handsfree_tutorials/script/2_base_control/*.py
+sudo chmod 777 ../handsfree_tutorials/script/3_navigation/*.py
+sudo chmod 777 ../handsfree_tutorials/script/4_visualization/*.py
+sudo chmod 777 ../handsfree_tutorials/script/5_advance_app/*.py
+sudo chmod 777 ../handsfree_tutorials/script/6_application/2_follow_me/*.py
+
